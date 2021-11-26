074f00c9217d22a131463f024a27c3c604370c7b
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2021-11-26 06:41:32 +0200

    initial code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div style={{ textAlign: 'center' }} className="App">
+            <h2>This is a full stack template: </h2>
+            <h1>React | Django</h1>
+        </div>
+    )
+}
+
+export default App
