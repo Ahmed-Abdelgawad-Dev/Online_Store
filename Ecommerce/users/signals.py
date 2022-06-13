51f963c24a7b6e42c4cea41daa009a08c8acb287
Ahmed-Abdelgawad-Dev <ahmed.abdelgawad.dev@gmail.com>
2022-06-14 01:16:21 +0200

    A signal for extracting a name for the user in case of no name written

---
+++ Ecommerce/users/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import CustomUser
+
+
+@receiver(pre_save, sender=CustomUser)
+def auto_rename_user(sender, instance, **kwargs):
+    user = instance
+    # Extract a name from the email in case user has no name
+    if user.name == '':
+        user.name = user.email.split("@")[0]
