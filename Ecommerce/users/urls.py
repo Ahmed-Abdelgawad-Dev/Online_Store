bfafae4a65f20215bdd5d93522609a8f7266947a
Ahmed-Abdelgawad-Dev <ahmed.abdelgawad.dev@gmail.com>
2022-06-14 00:41:10 +0200

    Register user endpoint & Funtionality

--- Ecommerce/users/urls.py
+++ Ecommerce/users/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
          name='token_obtain_pair'),
     path('get_user/', views.get_user, name="get_user"),
     path('get_users/', views.get_users, name="get_users"),
-
+    path('register_user/', views.register_user, name="register_user"),
 ]
--- Ecommerce/users/views.py
+++ Ecommerce/users/views.py
@@ -1,3 +1,5 @@
+from urllib import response
+from django.http import Http404
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
@@ -6,6 +8,8 @@ from .serializers import CustomUserSerializer, CustomUserSerializerWithToken
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django.contrib.auth.hashers import make_password
+from rest_framework.status import HTTP_404_NOT_FOUND
 
 
 @api_view(['GET'])
@@ -22,6 +26,23 @@ def get_users(request):
     users = CustomUser.objects.all()
     serialized_users = CustomUserSerializer(users, many=True)
     return Response(serialized_users.data)
+
+
+@api_view(['POST'])
+def register_user(request):
+    user_data = request.data
+    try:
+        created_user = CustomUser.objects.create(
+            email=user_data['email'],
+            name=user_data['name'],
+            password=make_password(user_data['password'])
+        )
+
+        serialized_user = CustomUserSerializerWithToken(
+            created_user, many=False)
+        return Response(serialized_user.data)
+    except:
+        return Response({"Attention": "The username is already existed, please choose a nother username"}, HTTP_404_NOT_FOUND)
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
