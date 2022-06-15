0554e090de23c7437f531c827da7f748d84c9563
Ahmed-Abdelgawad-Dev <ahmed.abdelgawad.dev@gmail.com>
2022-06-15 12:04:23 +0200

    Push last updates to repo

--- Ecommerce/orders/views.py
+++ Ecommerce/orders/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from urllib import response
+from django.http import Http404
+from .models import CustomUser, Order, ShippingAddress
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from django.contrib.auth.hashers import make_password
+from rest_framework.status import HTTP_404_NOT_FOUND
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def add_order_items(request):
+    user = request.user
+    data = request.data
+    order_items = data['order_items']
+    if order_items is not None and len(order_items) < 1:
+        return Response({"Attention": "You have not any order item yet"}, status=HTTP_404_NOT_FOUND)
+    else:
+        # Created Order
+        created_order = Order.objects.create(
+            name=data['name'],
+            user=user,
+            paymentMethod=data['paymentMethod'],
+            taxPrice=data['taxPrice'],
+            totalPrice=data['totalPrice'],
+            isPaid=data['isPaid'],
+            isDelivered=data['isDelivered'],
+            deliveredAt=data['deliveredAt']
+        )
+        # Created Shipping Adress.
+        shipping_data = data['shipping_address']
+        ShippingAddress.objects.create(
+            name=data['name'],
+            user=user,
+            order=created_order,
+            address=shipping_data['adress'],
+            city=shipping_data['city'],
+            postalCode=shipping_data['postalCode'],
+            country=shipping_data['country'],
+            shippingPrice=shipping_data['shippingPrice'],
+            telephone=shipping_data['shippingPrice']
+        )
